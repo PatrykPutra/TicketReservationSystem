773f9eebc154f171ebd179d81046c7e879851261
PatrykPutra <putryko@gmail.com>
2026-08-23 11:59:27 +0200

    ComandDispatcher tests refactored

--- TicketReservationSystem.Tests/CommandDispatcherTests.cs
+++ TicketReservationSystem.Tests/CommandDispatcherTests.cs
@@ -1,6 +1,5 @@
 using MediatR;
 using Moq;
-using TicketReservationSystem.Application.Abstractions;
 using TicketReservationSystem.Application.Commands;
 using TicketReservationSystem.Application.Commands.Tickets;
 using TicketReservationSystem.Domain.Entities;
@@ -13,16 +12,19 @@ public class CommandDispatcherTests
     [Fact]
     public async Task DispatchAsync_ForCommand_ForwardsToMediatorAndReturnsResult()
     {
-        var mediator = new Mock<IMediator>();
+        // Arrange
+        var mediatorMock = new Mock<IMediator>();
         var command = new TicketReservationCommand(TicketId.CreateUnique(), UserId.CreateUnique());
         var expected = TicketReservationResult.Success(command.TicketId, TicketStatus.Reserved, DateTime.UtcNow);
-        mediator.Setup(m => m.Send(command, It.IsAny<CancellationToken>())).ReturnsAsync(expected);
+        mediatorMock.Setup(m => m.Send(command, It.IsAny<CancellationToken>())).ReturnsAsync(expected);
 
-        var dispatcher = new CommandDispatcher(mediator.Object);
+        var dispatcher = new CommandDispatcher(mediatorMock.Object);
 
+        // Act
         var result = await dispatcher.DispatchAsync<TicketReservationCommand, TicketReservationResult>(command, CancellationToken.None);
 
+        // Assert
         Assert.Same(expected, result);
-        mediator.Verify(m => m.Send(command, It.IsAny<CancellationToken>()), Times.Once);
+        mediatorMock.Verify(m => m.Send(command, It.IsAny<CancellationToken>()), Times.Once);
     }
 }
