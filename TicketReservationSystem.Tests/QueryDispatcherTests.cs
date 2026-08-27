7bcd28dee6b3fd15935744e1b22bc9694c624195
PatrykPutra <putryko@gmail.com>
2026-08-27 11:00:18 +0200

    QueryDispatcher tests refactored

--- TicketReservationSystem.Tests/QueryDispatcherTests.cs
+++ TicketReservationSystem.Tests/QueryDispatcherTests.cs
@@ -1,6 +1,5 @@
 using MediatR;
 using Moq;
-using TicketReservationSystem.Application.Abstractions;
 using TicketReservationSystem.Application.DTOs;
 using TicketReservationSystem.Application.Queries;
 using TicketReservationSystem.Application.Queries.Events;
@@ -10,8 +9,9 @@ namespace TicketReservationSystem.Tests;
 public class QueryDispatcherTests
 {
     [Fact]
-    public async Task ExecuteAsync_ForQuery_ForwardsToMediatorAndReturnsResult()
+    public async Task ExecuteAsync_WhenInvoked_CallsMediatorSend()
     {
+        // Arrange
         var mediator = new Mock<IMediator>();
         var query = new GetEventsQuery();
         var expected = new GetEventsResult(new List<EventDto>());
@@ -19,9 +19,28 @@ public class QueryDispatcherTests
 
         var dispatcher = new QueryDispatcher(mediator.Object);
 
+        // Act
         var result = await dispatcher.ExecuteAsync<GetEventsQuery, GetEventsResult>(query, CancellationToken.None);
 
-        Assert.Same(expected, result);
+        // Assert
         mediator.Verify(m => m.Send(query, It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task ExecuteAsync_WhenInvoked_ReturnsResult()
+    {
+        // Arrange
+        var mediator = new Mock<IMediator>();
+        var query = new GetEventsQuery();
+        var expected = new GetEventsResult(new List<EventDto>());
+        mediator.Setup(m => m.Send(query, It.IsAny<CancellationToken>())).ReturnsAsync(expected);
+
+        var dispatcher = new QueryDispatcher(mediator.Object);
+
+        // Act
+        var result = await dispatcher.ExecuteAsync<GetEventsQuery, GetEventsResult>(query, CancellationToken.None);
+
+        // Assert
+        Assert.Same(expected, result);
     }
 }
