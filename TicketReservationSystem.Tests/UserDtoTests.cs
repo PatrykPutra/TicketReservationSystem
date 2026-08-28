8a72061ebac8b0210c044241811fc30c05a014ca
PatrykPutra <putryko@gmail.com>
2026-08-28 13:00:14 +0200

    UserDto tests refactored

--- TicketReservationSystem.Tests/UserDtoTests.cs
+++ TicketReservationSystem.Tests/UserDtoTests.cs
@@ -5,18 +5,20 @@ namespace TicketReservationSystem.Tests;
 
 public class UserDtoTests
 {
-    [Fact]
-    public void UserDto_ForValidInputs_ExposesAllFields()
+    [Theory]
+    [InlineData("user@test.com", "Test", "User", "123456789", true)]
+    [InlineData("test@email.com", "TestUser", "UserTest", "987654321", false)]
+    public void UserDto_ForValidInputs_ExposesAllFields(string email, string firstName, string lastName, string phoneNumber, bool isVerified)
     {
         var id = UserId.CreateUnique();
 
-        var dto = new UserDto(id, "user@test.com", "Jan", "Kowalski", "123456789", true);
+        var dto = new UserDto(id, email, firstName, lastName, phoneNumber, isVerified);
 
         Assert.Equal(id, dto.Id);
-        Assert.Equal("user@test.com", dto.Email);
-        Assert.Equal("Jan", dto.FirstName);
-        Assert.Equal("Kowalski", dto.LastName);
-        Assert.Equal("123456789", dto.PhoneNumber);
-        Assert.True(dto.IsVerified);
+        Assert.Equal(email, dto.Email);
+        Assert.Equal(firstName, dto.FirstName);
+        Assert.Equal(lastName, dto.LastName);
+        Assert.Equal(phoneNumber, dto.PhoneNumber);
+        Assert.Equal(isVerified, dto.IsVerified);
     }
 }
